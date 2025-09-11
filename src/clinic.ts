8ccd12adffd9e2a4b92956b4c5b4d14b617fda16
kcheverie <kcheverie@gmail.com>
2025-09-10 20:10:36 -0400

    initialize clinic with appointment types and opening/closing hous

--- src/clinic.ts
+++ src/clinic.ts
@@ -1,11 +1,33 @@
 const OPENING_TIME = 9
 const CLOSING_TIME = 17
 
-export function validateBooking(startTime: Date, endTime: Date): boolean {
+export type AppointmentType = {
+  id: string;
+  name: "Appointment" | "Consultation" | "Check-In";
+  length: number;
+}
+
+export class Clinic {
+  private openingTime: number;
+  private closingTime: number;
+  private appointmentTypes: AppointmentType[];
+
+  constructor(openingTime: number, closingTime: number) {
+    this.openingTime = openingTime;
+    this.closingTime = closingTime;
+    this.appointmentTypes = [
+      { id: 'appt', name: "Appointment", length: 60 },
+      { id: 'consult', name: "Consultation", length: 90 },
+      { id: 'checkin', name: "Check-In", length: 30 },
+    ] 
+  }
+
+
+  validateBooking(startTime: Date, endTime: Date): boolean {
     const start = startTime.getHours()
     const end = endTime.getHours()
     
       return start < end && start >= OPENING_TIME && end <= CLOSING_TIME
-    
 
+  }
 }
--- tests/clinic.test.ts
+++ tests/clinic.test.ts
@@ -1,9 +1,12 @@
-import { validateBooking } from '../src/clinic';
+import { Clinic } from '../src/clinic';
 
+
+let clinic: Clinic;
 let start: Date;
 let end: Date;
 
 beforeEach(() => {
+  clinic = new Clinic(9, 17) //initialize Clinic Hours
   start = new Date();
   end = new Date();
 })
@@ -11,30 +14,32 @@ beforeEach(() => {
 test('a 9am start time and 10am end time is valid', () => {
   start.setHours(9, 0, 0, 0);
   end.setHours(10, 0, 0, 0);
-  expect(validateBooking(start, end)).toBe(true);
+  expect(clinic.validateBooking(start, end)).toBe(true);
 });
 
 test('a 7pm start time is invalid', () => {
   start.setHours(19, 0, 0, 0);
   end.setHours(20, 0, 0, 0);
-  expect(validateBooking(start, end)).toBe(false);
+  expect(clinic.validateBooking(start, end)).toBe(false);
 });
 
 test('a 5pm end time is valid', () => {
   start.setHours(16, 0, 0, 0);
   end.setHours(17, 0, 0, 0);
-  expect(validateBooking(start, end)).toBe(true);
+  expect(clinic.validateBooking(start, end)).toBe(true);
 });
 
 
 test('a 5:30pm end time is ivalid', () => {
   start.setHours(17, 30, 0, 0);
   end.setHours(18, 0, 0, 0);
-  expect(validateBooking(start, end)).toBe(false);
+  expect(clinic.validateBooking(start, end)).toBe(false);
 });
+
+// add test for 4:00pm start time and 5:30pm end time
 
 test('an end time can not be earlier than a start time', () => {
   start.setHours(3, 30, 0, 0);
   end.setHours(1, 0, 0, 0);
-  expect(validateBooking(start, end)).toBe(false);
+  expect(clinic.validateBooking(start, end)).toBe(false);
 });
