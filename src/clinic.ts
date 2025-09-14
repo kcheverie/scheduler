01d37deb310e5e28b42083b58153b7347983fb81
kcheverie <kcheverie@gmail.com>
2025-09-14 15:17:27 -0400

    refactor booking validation

--- src/clinic.ts
+++ src/clinic.ts
@@ -1,5 +1,9 @@
 import type { AppointmentType, AppointmentSlot, Booking } from './models'
 
+type BookingValidation = {
+  valid: boolean;
+  reason?: string;
+}
 
 export class Clinic {
   openingTime: number;
@@ -19,14 +23,6 @@ export class Clinic {
     this.nowFn = nowFn; 
     this.bookings = [];
   }
-
-  validateBooking(startTime: Date, endTime: Date): boolean {
-    // convert times to decimals if needed ie 5:30 -> 17.5
-    const start = startTime.getHours() + startTime.getMinutes() / 60
-    const end = endTime.getHours() + endTime.getMinutes() / 60
-    return start < end && start >= this.openingTime && end <= this.closingTime
-  }
-
 
   getAppointmentSlots(startDate: Date, endDate: number): AppointmentSlot[] {
     const slots: AppointmentSlot[] = []
@@ -49,60 +45,74 @@ export class Clinic {
       }
 
     }
-
     return slots;
   }
-  
+
   clearBookings() {
     this.bookings = [];
   }
 
-  createBooking(date: Date, patientId: string, appointmentTypeId: string): Booking {
+  isBookingValid(start: Date, end: Date): BookingValidation {
+    // convert times to decimals if needed ie 5:30 -> 17.5
+    const startTime = start.getHours() + start.getMinutes() / 60
+    const endTime = end.getHours() + end.getMinutes() / 60
+  
+    if (startTime < this.openingTime || endTime > this.closingTime || startTime >= endTime) {
+      return { valid: false, reason: 'can not create a booking outside clinic hours' };
+    }
+     
     const now = this.nowFn();
-    //stub test
-    if (!date || !patientId || !appointmentTypeId) {
-      throw new Error('invalid booking data')
-    }
 
-    let appointmentType = this.appointmentTypes.find((appointmentType) => appointmentType.id === appointmentTypeId)
-    
-    if (!appointmentType) throw new Error('invalid appointment type')
-    
-    const bookingEnd = new Date(date.getTime() + appointmentType.length * 60 * 1000)
-
-    if (!this.validateBooking(date, bookingEnd)) {
-      throw new Error('can not create a booking outside clinic hours')
-    }
-
-    if (date.getTime() < now.getTime()) {
-      throw new Error('can not create a booking in the past')
+    if (start.getTime() < now.getTime()) {
+      return { valid: false, reason: 'can not create a booking in the past' }
     }
 
     const twoHoursFromNow = new Date(now);
     twoHoursFromNow.setHours(now.getHours() + 2)
-
-    if (date.getTime() < twoHoursFromNow.getTime()) {
-      throw new Error('can not create a booking in the next two hours')
+    if (start.getTime() < twoHoursFromNow.getTime()) {
+      return { valid: false, reason: 'can not create a booking in the next two hours' }
     }
 
+    return {valid: true} //if all checks have passed
+  }
 
 
-    const newBooking = {
-      id: 'stub-id', 
-      patientId,
-      appointmentTypeId,
-      start: date,
-      end: new Date(date.getTime() + appointmentType.length * 60 * 1000)
+  createBooking(date: Date, patientId: string, appointmentTypeId: string): Booking {
+    if (!date || !patientId || !appointmentTypeId) {
+      throw new Error('invalid booking data')
     }
+    
 
+    let appointmentType = this.appointmentTypes.find((appointmentType) => appointmentType.id === appointmentTypeId)
+    
+    if (!appointmentType) throw new Error('invalid appointment type')
+    const bookingEnd = new Date(date.getTime() + appointmentType.length * 60 * 1000)
+    
+    
+    const validation = this.isBookingValid(date, bookingEnd)
+    
     const overlaps = this.bookings.some(booking => 
-      newBooking.start < booking.end && newBooking.end > booking.start
+      date < booking.end && bookingEnd > booking.start
     )
+    if (overlaps) {
+      throw new Error("can not create a booking that overlaps with another booking")
+    }
 
-    if (overlaps) throw new Error ("can not create a booking that overlaps with another booking")
-
-    this.bookings.push(newBooking)
-    return newBooking
+    if (validation.valid) {
+      const newBooking = {
+        id: 'stub-id', 
+        patientId,
+        appointmentTypeId,
+        start: date,
+        end: new Date(date.getTime() + appointmentType.length * 60 * 1000)
+      }
+  
+      this.bookings.push(newBooking)
+      return newBooking
+    } else {
+      throw new Error(validation.reason)
+    }
   }
 
 }
+
--- tests/clinic.test.ts
+++ tests/clinic.test.ts
@@ -6,7 +6,7 @@ let start: Date;
 let end: Date;
 let today: Date;
 
-describe('Clinic', () => {
+describe('isBookingValid', () => {
   beforeEach(() => {
     today = new Date("2025-09-08T13:00:00") //make sure "today" is always september 8 at 1pm
     clinic = new Clinic(9, 17, () => today) //initialize Clinic Hours and today's date
@@ -15,42 +15,64 @@ describe('Clinic', () => {
   })
 
   test('a 9am start time and 10am end time is valid', () => {
-    start.setHours(9, 0, 0, 0);
-    end.setHours(10, 0, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(true);
+    start = new Date("2025-09-09T09:00:00");
+    end = new Date("2025-09-09T10:00:00");
+    const validation = clinic.isBookingValid(start, end)
+    console.log(validation)
+    expect(validation.valid).toBe(true);
   });
   
   test('a 7pm start time is invalid', () => {
-    start.setHours(19, 0, 0, 0);
-    end.setHours(20, 0, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(false);
+    start = new Date("2025-09-09T19:00:00");
+    end = new Date("2025-09-09T20:00:00");
+    const validation = clinic.isBookingValid(start, end)
+    expect(validation.valid).toBe(false);
   });
   
   test('a 5pm end time is valid', () => {
-    start.setHours(16, 0, 0, 0);
-    end.setHours(17, 0, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(true);
+    start = new Date("2025-09-09T16:00:00");
+    end = new Date("2025-09-09T17:00:00");
+    const validation = clinic.isBookingValid(start, end)
+    expect(validation.valid).toBe(true);
   });
   
   
-  test('a 5:30pm end time is ivalid', () => {
-    start.setHours(17, 30, 0, 0);
-    end.setHours(18, 0, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(false);
+  test('a 5:30pm end time is invalid', () => {
+    start = new Date("2025-09-09T17:30:00");
+    end = new Date("2025-09-09T18:00:00");
+    const validation = clinic.isBookingValid(start, end)
+    expect(validation.valid).toBe(false);
   });
   
   
   test('an 4pm start time and a 5:30 pm end time is invalid', () => {
-    start.setHours(16, 0, 0, 0);
-    end.setHours(17, 30, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(false);
+    start = new Date("2025-09-09T16:00:00");
+    end = new Date("2025-09-09T17:30:00");
+    const validation = clinic.isBookingValid(start, end)
+    expect(validation.valid).toBe(false);
   });
   
   test('an end time can not be earlier than a start time', () => {
-    start.setHours(3, 30, 0, 0);
-    end.setHours(1, 0, 0, 0);
-    expect(clinic.validateBooking(start, end)).toBe(false);
+    start = new Date("2025-09-09T15:00:00");
+    end = new Date("2025-09-09T14:00:00");
+    const validation = clinic.isBookingValid(start, end)
+    expect(validation.valid).toBe(false);
   });
+
+  test('a start time in the past is invalid', () => {
+    start = new Date(today.getTime() + 60 * 60 * 1000); // +1 hour
+    end = new Date(start.getTime() + 60 * 60 * 1000);
+    const validation = clinic.isBookingValid(start, end);
+    expect(validation.valid).toBe(false);
+    expect(validation.reason).toBe('can not create a booking in the next two hours');  
+  })
+
+  test('can not create booking that begins in the next two hours', () => {
+    start = new Date(today.getTime() + 2 * 60 * 60 * 1000); // +2 hours
+    end = new Date(start.getTime() + 60 * 60 * 1000);
+    const validation = clinic.isBookingValid(start, end);
+    expect(validation.valid).toBe(true);
+  })
 
 })
 
@@ -98,7 +120,6 @@ describe('createBooking', () => {
       const date = new Date("2025-09-09T15:30:00") 
       const booking = clinic.createBooking(date, 'patient01', 'checkin')
      
-      console.log("BOOKING", booking.start)
       expect(booking.start.getTime()).toBe(date.getTime());
       const expectedEnd = new Date(date.getTime() + 30 * 60 * 1000)
       expect(booking.end.getTime()).toBe(expectedEnd.getTime())
@@ -135,29 +156,6 @@ describe('createBooking', () => {
   })
 
   describe('unsuccessful bookings', () => {
-
-    test('can not creating a booking in the past', () => {
-      const dateInPast = new Date("2025-09-08T10:00:00") 
-      expect(() => clinic.createBooking(dateInPast, 'patient01', 'appt')).toThrow('can not create a booking in the past')
-    })
-  
-    test('can not create booking that begins in the next two hours', () => {
-      const dateNextTwoHours = new Date("2025-09-08T14:00:00") 
-      expect(() => clinic.createBooking(dateNextTwoHours, 'patient01', 'appt')).toThrow('can not create a booking in the next two hours')
-    })
-  
-    test('can not create a booking before clinic hours', () => {
-      const dateEarly = new Date("2025-09-09T07:00:00") 
-      expect(() => clinic.createBooking(dateEarly, 'patient01', 'appt')).toThrow('can not create a booking outside clinic hours')
-
-    })
-  
-    test('can not create a booking after clinic hours', () => {
-      const dateLate = new Date("2025-09-09T20:00:00") 
-      expect(() => clinic.createBooking(dateLate, 'patient01', 'appt')).toThrow('can not create a booking outside clinic hours')
-
-    })
-  
     test('can not create a booking that overlaps with another booking', () => {
       //create booking for 4:00pm
       const dateFirstAppt = new Date("2025-09-09T16:00:00") 
@@ -174,4 +172,4 @@ describe('createBooking', () => {
       expect(() => clinic.createBooking(date, 'patient01', 'appointment')).toThrow('invalid appointment type')
     })
   })
-});
+};
